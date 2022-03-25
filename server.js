19ba39b4fba75613622b62b36d2957e577ef366e
Emmanuel Lyimo <esal29t@gmail.com>
2022-03-25 04:55:50 -0700

    added starter files from class demos

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
